d1fce3e7323264543b9aab89c89ac4c57b5c2a46
Githubuzer1237 <158813082+Githubuzer1237@users.noreply.github.com>
2024-10-19 20:00:26 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+  <>
+<header className="header">
+   <div className="container">
+      <nav className="nav">
+         <img src="https://img.freepik.com/free-vector/bird-colorful-gradient-design-vector_343694-2506.jpg?size=338&ext=jpg&ga=GA1.1.967060102.1729296000&semt=ais_hybrid" alt="" className="logo" />
+         <div className="links">
+         <a href="">okak</a>
+         <a href="">okak</a>
+         <a href="">okak</a>
+         <a href="">okak</a>
+
+         </div>
+<div className="text">
+   <a href="">okak</a>
+   <a href="">okak</a>
+
+ 
+</div>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png" alt="" className="burger" />
+      </nav>
+    
+   </div>
+</header>
+  </>
+)
+}
+
+export default App
